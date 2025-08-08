6945d93660928fd20cbf101a0a4491b1810d9ba7
Arslan <devarslan457@gmail.com>
2025-08-08 16:12:44 +0530

    add admin panel design

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -1,10 +1,22 @@
 import Navbar from "./components/Navbar";
+import SidebarPanel from "../src/components/sidebarPanel"
 
 const App = () =>{
   return (
-    <>
-      <Navbar></Navbar>
-    </>
+    <div className="bg-grey-50 min-h-screen">
+      <>
+         <Navbar></Navbar>
+         <hr />
+
+         <div className="flex w-full">
+
+          <SidebarPanel></SidebarPanel>
+
+         </div>
+
+      </>
+ 
+    </div>
   )
 } ;
 
--- admin/admin/src/components/sidebarPanel.jsx
+++ admin/admin/src/components/sidebarPanel.jsx
@@ -1,6 +1,26 @@
- const SidebarPanel = () =>{
+
+import {NavLink} from "react-router-dom" ;
+
+const SidebarPanel = () =>{
     return (
-        <div>
+        <div className="w-[18%] min-h-screen  border-r-1">
+
+            <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[15px]">
+                <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/add">
+                   <span class="material-symbols-outlined">add_box</span>
+                <p className="hidden md:block">Add Items</p>
+                </NavLink>
+
+                               <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/list">
+                   <span class="material-symbols-outlined">order_approve</span>
+                <p className="hidden md:block">List Items</p>
+                </NavLink>
+
+                               <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/orders">
+                   <span class="material-symbols-outlined">order_approve</span>
+                <p className="hidden md:block">Order</p>
+                </NavLink>
+            </div>
 
         </div>
     )
